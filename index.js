30aee13d56c35546ca6753e1827bf20740c581da
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-04-18 11:26:36 -0300

    01:06 - Avançando na utilização do import

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-import { adicao, subtracao, multiplicacao, divisao } from './calculadora.js';
+// import { adicao, subtracao, multiplicacao, divisao } from './calculador.js';
+
+// import * as calculador from './calculador.js';
+
+import raizQuadrada,
+{
+    adicao as soma,
+    subtracao,
+    multiplicacao,
+    divisao
+} from './calculador.js';
 
 const a = 10;
 const b = 5;
 
-console.log(`Adição: ${a} + ${b} = ${adicao(a, b)}`);
+console.log(`Adição: ${a} + ${b} = ${soma(a, b)}`);
 console.log(`Subtração: ${a} - ${b} = ${subtracao(a, b)}`);
 console.log(`Multiplicação: ${a} * ${b} = ${multiplicacao(a, b)}`);
 console.log(`Divisão: ${a} / ${b} = ${divisao(a, b)}`);
+
+console.log(raizQuadrada(5));
+
