a586ed96ee862f82bc275431af94763a3105d684
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-04-18 11:26:12 -0300

    01:05 - Avançando na utilização do export

--- calculadora.js
+++ calculadora.js
@@ -2,3 +2,22 @@ export const adicao = (a, b) => a + b;
 export const subtracao = (a, b) => a - b;
 export const multiplicacao = (a, b) => a * b;
 export const divisao = (a, b) => a / b;
+
+const PI = 3.14;
+
+class Circulo { }
+
+
+export {
+    PI,
+    Circulo
+}
+
+export default function raizQuadrada(x) {
+    return Math.sqrt(x);
+}
+
+export {
+    PI as constantePI,
+    Circulo as circulo
+}
